9817a5f9a0ad1fcddba3866c03ecfec4d4bb5399
BatuCan <batucankarakoyunlu@gmail.com>
2026-03-28 23:00:47 +0300

    removed old dev file version

--- SorcererShowdown - Source/code/DevTools/DevFile.cpp
+++ SorcererShowdown - Source/code/DevTools/DevFile.cpp
@@ -1,48 +1,43 @@
 #include "Sorcerer.h"
-#include "Techniques.h"
-#include "Shikigami.h"
-#include "Specials.h"
-#include "Domain.h"
-#include "Utils.h"
+#include "Shrine.h"
+#include "WorldCuttingSlash.h"
+#include "SimpleDomain.h"
+#include "DevFile.h"
+#include "ShikigamiList.h"
 
 import std;
 
-test_sorcerer::test_sorcerer() : Sorcerer(30000000.0, 30000000.0, 1000.0) {
-    technique = std::make_unique<Copy>();
+test_sorcerer::test_sorcerer() : Sorcerer(INT32_MAX, INT32_MAX, 0.0) {
+    technique = std::make_unique<Shrine>();
+    special = std::make_unique<WorldCuttingSlash>();
     domain = std::make_unique<KillEveryoneDomain>();
     counter_domain = std::make_unique<SimpleDomain>();
+    shikigami.push_back(std::make_unique<Rika>());
+    shikigami.push_back(std::make_unique<Agito>());
+    shikigami.push_back(std::make_unique<Mahoraga>());
     black_flash_chance = 100;
 }
 
 void test_sorcerer::OnSorcererTurn(std::vector<std::unique_ptr<Sorcerer>>& battlefield) {
-    std::println("John Zenin just stands there... menacingly.");
-    if (this->domain && !this->DomainActive() && !(this->GetTechnique()->BurntOut() || this->GetTechnique()->Boosted())) {
-        this->ActivateDomain();
-    }
-    for (const auto& s : battlefield) {
-        if (s.get() == this) continue;
-        this->Taunt(s.get());
-    }
+    std::println("Exception thrown");
+    std::println("Access violation reading location 0x000000000000000C.");
 }
 std::string test_sorcerer::GetName() const {
-    return "John Zenin";
+    return "NullPointer";
 }
 
 bool test_sorcerer::CanBeHit() const {
     return true;
 }
 
-// Domain
-
 KillEveryoneDomain::KillEveryoneDomain() : Domain(10000000.0, 10000000.0, 1000.0) {}
 
 void KillEveryoneDomain::OnSureHit(Character& target) {
-    if (clashing) return;
     target.DamageBypass(surehit_killdamage);
-    std::println("{} got hit by {}'s SureHit!", target.GetName(), this->GetDomainName());
+    std::println("{} got erased by {}'s SureHit!", target.GetName(), this->GetDomainName());
 }
 std::string KillEveryoneDomain::GetDomainName() const {
-    return "KillEveryone Domain 3000";
+    return "Hard-Coded Inevitability";
 }
 
 double KillEveryoneDomain::GetUseCost() const {
