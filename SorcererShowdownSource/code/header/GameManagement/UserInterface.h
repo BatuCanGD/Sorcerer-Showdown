108b260dfecd8265886792583f737b5ef2464b18
BatuCan <batucankarakoyunlu@gmail.com>
2026-05-15 13:07:14 +0300

    added updated header files

---
+++ SorcererShowdownSource/code/header/GameManagement/UserInterface.h
@@ -0,0 +1,11 @@
+#pragma once
+class Sorcerer;
+class Character;
+
+class UserInterface {
+public:
+	void ShowBattleEntry(const std::vector<std::unique_ptr<Character>>& battlefield);
+	void DisplaySorcererStatus(Character* s);
+	static void ContinuePrompt(bool);
+	static void ClearScreen();
+};
