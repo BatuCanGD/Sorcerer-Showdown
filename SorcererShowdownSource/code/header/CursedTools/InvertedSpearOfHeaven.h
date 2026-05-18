ace8701af737341c6c43ea7d016660d8c5301a7d
BatuCan <batucankarakoyunlu@gmail.com>
2026-05-18 12:36:08 +0300

    added updated headers

--- SorcererShowdownSource/code/header/CursedTools/InvertedSpearOfHeaven.h
+++ SorcererShowdownSource/code/header/CursedTools/InvertedSpearOfHeaven.h
@@ -3,7 +3,7 @@
 
 class InvertedSpearofHeaven : public CursedTool {
 private:
-	double special_tool_damage = base_tool_damage * 2.25;
+	double tool_damage = 10.0 * 2.25;
 public:
 	InvertedSpearofHeaven();
 	std::unique_ptr<CursedTool> Clone() const override;
--- SorcererShowdownSource/code/header/CursedTools/Katana.h
+++ SorcererShowdownSource/code/header/CursedTools/Katana.h
@@ -2,6 +2,8 @@
 #include "code/header/CursedTools/CursedTool.h"
 
 class Katana : public CursedTool {
+protected:
+	double base_damage = 25.0;
 public:
 	Katana();
 	std::unique_ptr<CursedTool> Clone() const override;
